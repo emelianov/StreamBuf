42e3adc0f7a969c01ae35a34a111f59dffd0e5e9
Alexander Emelianov <a.m.emelianov@gmail.com>
2019-12-02 11:20:22 +0300

    Add DuplexBuf for bidirectional data flow simulation

--- StreamBuf.h
+++ StreamBuf.h
@@ -1,53 +1,94 @@
 //
-// Simple class to read data from buffer as it was Stream
+// Simple class to read/write data from/to buffer as it was a Stream
 //
-// (c)2017, Alexander Emelianov (a.m.emelianov@gmail.com)
+// (c)2017,2019 Alexander Emelianov (a.m.emelianov@gmail.com)
 //
 
 #pragma once
 
 class StreamBuf : public Stream {
   public:
-  StreamBuf() : Stream() {
-    buff = NULL;
-    length = 0;
-    pos = 0;
+  StreamBuf(size_t len = 0) : Stream() {
+    if (len) {
+      buff = (uint8_t*)malloc(len);
+      if (buff)
+        length = len;
+    }
   }
-  StreamBuf(uint8_t* src, size_t len) : Stream() {
+  StreamBuf(uint8_t* src, size_t len, bool filled = false) : Stream() {
     buff = src;
     length = len;
-    pos = 0;
-  }
-  void open(uint8_t* src, size_t len) {
-    buff = src;
-    length = len;
-    pos = 0;
-    //Serial.println(len);    
+    if (filled)
+      wpos = len - 1;
   }
   void reset() {
-    pos = 0;
+    return;
+    rpos = 0;
+    wpos = 0;
   }
   int read() {
-    if (pos >= length) return -1;
-    uint8_t t = buff[pos];
-    pos++;
+    int t = peek();
+    if (t >= 0)
+      rpos++;
     return t;
   }
   int available() {
-    if (pos >= length) return -1;
-    return length - pos;
+    if (rpos >= wpos) return 0;
+    return wpos - rpos;
   }
   int peek() {
-    return -1;
+    if (rpos >= wpos) return -1;
+    return buff[rpos];
   }
   size_t write(uint8_t b) {
-    return -1;
+    if (wpos >= length) {
+      if (rpos == 0)
+        return 0;
+      memcpy(buff, buff+rpos, wpos - rpos);
+      wpos -= rpos;
+      rpos = 0;
+    }
+    buff[wpos] = b;
+    wpos++;
+    return 1;
   }
   void flush() {
-    pos = length;
+    return;
   }
   private:
-  size_t pos;
-  uint8_t* buff;
-  size_t length;
+  size_t rpos = 0;
+  size_t wpos = 0;
+  uint8_t* buff = nullptr;
+  size_t length = 0;
+};
+
+class DuplexBuf : public Stream {
+public:
+  DuplexBuf(Stream* rx, Stream* tx) {
+    _rx = rx;
+    _tx = tx;
+  }
+  /*
+  void reset() {
+    _rx->reset();
+    _tx_>reset();
+  }*/
+  int read() {
+    return _rx->read();
+  }
+  int available() {
+    return _rx->available();
+  }
+  int peek() {
+    return _rx->peek();
+  }
+  size_t write(uint8_t b) {
+    return _tx->write(b);
+  }
+  void flush() {
+    return;
+  }
+private:
+  Stream* _rx;
+  Stream* _tx;
 };
